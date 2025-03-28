fed9ade8a410c8d743ac125309603add7b588d76
dham <fullstackcodingguy@gmail.com>
2025-03-28 13:53:36 +0530

    first

---
+++ notebot-next/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
+        This is the main content of the page.
+
+        <div>
+      <Button>Click me</Button>
+    </div>
+      </main>
+    
+    </div>
+  );
+}
